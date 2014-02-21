de5752c8a4d0158c9d8d279de2379ff503144550
xavoc <xavoc@xavoc-desktop.(none)>
2014-02-21 12:43:16 +0530

    REcreated models

---
+++ page/owner/branch.php
@@ -0,0 +1,13 @@
+<?php
+class page_hotelERPApp_page_owner_branch extends page_componentBase_page_owner_main{
+	function init(){
+		parent::init();
+		$branch_crud=$this->add('CRUD'); 
+		$branch_crud->setModel('hotelERPApp/Master_Branch');
+		if($branch_crud->grid)
+		{
+
+			$branch_crud->add_button->set('Add Branch Details');
+		}
+	}
+}
---
+++ page/owner/hotel.php
@@ -0,0 +1,13 @@
+<?php
+class page_hotelERPApp_page_owner_hotel extends page_componentBase_page_owner_main{
+	function init(){
+		parent::init();
+		$hotel_crud=$this->add('CRUD'); 
+		$hotel_crud->setModel('hotelERPApp/Master_Hotel');
+		if($hotel_crud->grid)
+		{
+
+			$hotel_crud->add_button->set('Add Branch');
+		}
+	}
+}
