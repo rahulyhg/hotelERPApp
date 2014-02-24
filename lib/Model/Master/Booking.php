8cd4f5f56939a99a4b5f2bfbe64a349a92e2595b
Navjeet <navjeet@navjeet-TravelMate-4335.(none)>
2014-02-24 13:14:40 +0530

    add fields

--- lib/Model/Master/Booking.php
+++ lib/Model/Master/Booking.php
@@ -5,7 +5,16 @@ class Model_Master_Booking extends \Model_Table
 	public $table='hotelERPApp_booking';
 	function init(){
 		parent::init();
-		$this->addField('name')->caption('Name');
+		$this->addField('name')->caption('Customer Name');
+		$this->addField('id_no')->caption('Id Card Number');
+		$this->addField('gender')->caption('Gender');
+		$this->addField('date')->caption('Date of Booking');
+		$this->addField('room_no')->caption('Room Number');
+		$this->addField('address')->caption('Address');
+		$this->addField('contact')->caption('Contact');
+		$this->addField('email')->caption('Email-Id');
+		$this->addField('from')->caption('From');
+		$this->addField('to')->caption('To');
 
 	    $this->hasMany('hotelERPApp/Master_Bookingcustomer','hotelERPApp_booking_id');
 	}
--- lib/Model/Master/Checkin.php
+++ lib/Model/Master/Checkin.php
@@ -5,7 +5,7 @@ class Model_Master_Checkin extends \Model_Table
 	public $table='hotelERPApp_checkin';
 	function init(){
 		parent::init();
-		$this->addField('name')->caption('Name');
+		
 
 	    $this->hasMany('hotelERPApp/Master_Customer','hotelERPApp_checkin_id');
 	}
