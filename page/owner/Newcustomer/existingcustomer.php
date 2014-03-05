c789c630be944ecf9353d1a4414d4f01827d17e2
Navjeet <navjeet@navjeet-TravelMate-4335.(none)>
2014-03-05 11:01:06 +0530

    added page

--- page/owner/Newcustomer/existingcustomer.php
+++
@@ -1,9 +0,0 @@
-<?php
-class page_hotelERPApp_page_owner_Newcustomer_guestbook extends page_componentBase_page_owner_main{
-	function init(){
-		parent::init();
-		$crud=$this->add('CRUD'); 
-		$crud->setModel('hotelERPApp/Customer_Guestbook');
-		
-	}
-}
--- page/owner/main.php
+++ page/owner/main.php
@@ -2,18 +2,15 @@
 class page_hotelERPApp_page_owner_main extends page_componentBase_page_owner_main{
 	function init(){
 		parent::init();
-		// $tabhotel=$this->add('Tabs');
-		// $tabhotel->addTabURL('hotelERPApp_page_owner_hotel','Hotel');
-		// $tabhotel->addTabURL('hotelERPApp_page_owner_branch','Branch');
 		$menu=$this->add('Menu',null);
 
             $menu->addMenuItem('hotelERPApp_page_owner_hotel','Hotel');
             $menu->addMenuItem('hotelERPApp_page_owner_branch','Branch');
-<<<<<<< HEAD
+
             $menu->addMenuItem('hotelERPApp_page_owner_Newcustomer_guestbook','New Customer');
-=======
+
             $menu->addMenuItem('hotelERPApp_page_owner_customer','New Customer');
->>>>>>> 7b764f002fd038d1090c0852fb27b333379194c9
+
 		
 	}
 }
